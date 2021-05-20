2c31036f6aa37392bd6bb2cc54dc149eec82d921
ianubos <1031gonshi@gmail.com>
2021-05-20 18:06:46 +0900

    added node

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.enable('trust proxy')
+app.use(cors({}))
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+    res.send('<h1>hi there</h1>')
+    console.log('/ root is active.')
+})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`listening on port ${port}`))
